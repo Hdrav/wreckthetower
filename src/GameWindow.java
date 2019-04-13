808525ae517873041a2f93c2d825eb012b6ecd5b
simone <simo.kun.san@gmail.com>
2019-04-13 18:09:45 +0200

    start implementation of game window

---
+++ src/GameWindow.java
@@ -0,0 +1,20 @@
+
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+
+public class GameWindow extends Application{
+
+	
+	public void start(Stage stage) throws Exception {
+		
+		Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/main.fxml")) ;
+		
+	}
+	
+}
